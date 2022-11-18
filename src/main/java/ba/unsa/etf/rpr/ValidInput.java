a788647cb3a7f53cb2950dcca4de8b34d0a05aa2
tvelic1 <tvelic1@etf.unsa.ba>
2022-11-18 14:25:39 +0100

    first commit

--- src/main/java/ba/unsa/etf/rpr/ValidInput.java
+++ src/main/java/ba/unsa/etf/rpr/ValidInput.java
@@ -6,14 +6,14 @@ public class ValidInput {
         boolean valid=true;
         char prva=a.charAt(0);
         char last=a.charAt(a.length()-1);
-        if(prva!='(' || last != ')' ){
+        if(prva!='(' || last != ')' ){ //mora pocet zagradom i zavrsit zatvorenom zagradom
             valid=false;
             throw new RuntimeException("Invalid input");
         }
         for(int i=0;i<a.length();i++) {
 
             //najosnovnija provjera ne smije biti nista sto nije cifra ili operator ili razmak ili sqrt
-            if(a.charAt(i)!=')' && a.charAt(i)!=' ' && a.charAt(i)!='(' && a.charAt(i)!='s' && a.charAt(i)!='r' && a.charAt(i)!='q' && a.charAt(i)!='t' && isDigit(a.charAt(i))==false && isOperator(a.charAt(i))==false)
+            if(a.charAt(i)!=')' && a.charAt(i)!=' ' && a.charAt(i)!='(' && isSqrt(a.charAt(i))==false && isDigit(a.charAt(i))==false && isOperator(a.charAt(i))==false)
             {valid=false; throw new RuntimeException("Greska");}
 
 
@@ -34,14 +34,24 @@ public class ValidInput {
             { String pom=a.substring(a.indexOf("s")); //uzmi string od s pa nadalje
 
                 String pom2=pom.substring(pom.indexOf("s"),pom.indexOf(")")); // od s pa do prve zatvorene
-                int duzina_potkorijene_velicine=pom2.length()-6; //prebrojavanjem se dobije
-                for(int i=pom.indexOf("t")+2;i<pom.indexOf("t")+2+duzina_potkorijene_velicine;i++)
+                int duzina_potkorijene_velicine=pom2.length()-8; //prebrojavanjem se dobije
+                for(int i=pom.indexOf("t")+4;i<pom.indexOf("t")+4+duzina_potkorijene_velicine;i++)
                 {
                     if(isDigit(pom.charAt(i))==false) { valid=false; //da li je svaki element potkorijene velicine cifra
                         throw new RuntimeException("Invalid inputtt");}
                 }
 
-           }
+           }int br=0;
+            for(int i=0; i<a.length();i++)
+            {
+                if(a.charAt(i)=='(') br++;
+                else if(a.charAt(i)==')') br--;
+            }
+            if(br!=0){
+                valid=false;
+                throw new RuntimeException("Unmached"); //za zagrade
+
+            }
 
         return valid;
 
@@ -66,8 +76,8 @@ public class ValidInput {
         return false;
     }
     public static boolean isSqrt(char c){
-        if(c!='s' || c!='q' || c!='r' || c!='t') return false;
-        return true;
+        if(c=='s' || c=='q' || c=='r' || c=='t') return true;
+        return false;
     }
 
 
