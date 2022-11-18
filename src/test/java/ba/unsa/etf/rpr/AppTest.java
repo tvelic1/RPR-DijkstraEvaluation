a7f8c7d71a50169d01386df878c519b4c85da021
tvelic1 <tvelic1@etf.unsa.ba>
2022-11-18 17:59:23 +0100

    first commit

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,8 +1,10 @@
 package ba.unsa.etf.rpr;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
